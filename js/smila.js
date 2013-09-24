d9b0bbc43217005baff7f381ca8fed9dbda5b7f4
justayak <justayak@googlemail.com>
2013-09-24 19:15:44 +0200

    impl start

--- js/smila.js
+++ js/smila.js
@@ -63,6 +63,10 @@ window.Smila = function(){
     /**
      * Private Cache for DataStore
      * @type {Object}
+     * {
+     *      key : { data: { .. }, canvas: { HTMLCanvas } }
+     *      ...
+     * }
      */
     var spriteCache = {};
 
@@ -152,11 +156,62 @@ window.Smila = function(){
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     /**
-     * @param spriteData { ... }
-     * @param callback {function} (Sprite)
+     * @param spriteData
+     *{
+     *      src : "/res/image.png",   {String}
+     *      base64 : "base64/png...",   {String}
+     *      w : width : 64   {Integer}
+     *      h : height : 64   {Integer}
+     *      o : outline : true  {Boolean} // optional
+     *      bm : bitmask : true  {Boolean} // optional
+     *      key : "character"  {String}
+     * }
+     * @param callback {function}
+     * @param error {function}
      */
-    var loadSprite = function(spriteData, callback){
-        log("[Smila::*->loadSprite]");
+    var loadSprite = function(spriteData, callback, error){
+        log("[Smila::*->loadSprite] {" + spriteData.key + "}");
+        spriteData.w = spriteData.w | spriteData.width;
+        spriteData.h = spriteData.h | spriteData.height;
+        spriteData.o = spriteData.o | spriteData.outline | false;
+        spriteData.bm = spriteData.bm | spriteData.bitmask | false;
+
+        var img = new Image();
+        img.onload = function(){
+            log("[Smila::*->loadSprite] Image is loaded {" + spriteData.key + "}");
+            var canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var context = canvas.getContext('2d');
+            context.drawImage(img,0,0);
+
+            spriteCache[spriteData.key] = {meta:spriteData, canvas:canvas};
+            // ~~~ outline? ~~~
+            if( spriteData.o ){
+                var key = spriteData.key + "_outline";
+
+            }
+
+
+            callback();
+        };
+
+        img.onerror = function(){
+            log("[Smila::*->loadSprite] Could not load Image {" + spriteData.key + "}");
+            error();
+        };
+
+        if (spriteData.key in spriteCache){
+            log("[Smila::*->loadSprite] load from Cache: {" + spriteData.key + "}");
+            callback();
+        }else if (spriteData.base64 !== undefined){
+            log("[Smila::*->loadSprite] load from base64: {" + spriteData.key + "}");
+            img.src = spriteData.base64;
+        }else{
+            log("[Smila::*->loadSprite] load from URL: {" + spriteData.key + "}");
+            img.src = imagePath + spriteData.src;
+        }
+
     };
 
     var loadDataStoreFromLocalStorage = function(){
@@ -169,16 +224,6 @@ window.Smila = function(){
 
     var log = function(message){
         console.log(message);
-    };
-
-    /**
-     *
-     * @param spriteData
-     * @param successCallback {function}
-     * @param errorCallback {function}
-     */
-    var loadSprite = function(spriteData, successCallback, errorCallback){
-
     };
 
 
