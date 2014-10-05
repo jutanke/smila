e89fb493780dda52e9a22bcc535e283cd4a5bba7
Julian <jutanke@googlemail.com>
2014-10-05 03:21:15 +0200

    smila2: impl img loading

--- js/smila2.js
+++ js/smila2.js
@@ -8,6 +8,14 @@
  */
 window.Smila = function () {
 
+    if (!window.CanvasRenderingContext2D) {
+        throw "Smila-Rendering is not supported by the browser! (#1)";
+    }
+
+    if (!isDefined(performance) && !isDefined(performance.now)){
+        throw "Smila-Rendering is not supported by the browser! (#2)";
+    }
+
     var VERBOSE = true;
 
     function log(msg) {
@@ -16,18 +24,258 @@ window.Smila = function () {
         }
     }
 
+    function isDefined(obj) {
+        return typeof obj !== 'undefined';
+    }
+
+    var Smila = {};
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Renderer
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    var EXPECTED_ELAPSED_MILLIS = Math.floor(1000 / 60);
+
+    /**
+     *
+     * @params domId {String} where to put the canvas
+     * @params options {Object}
+     * {
+     *      trackMouse : true, {Boolean}
+     * }
+     *
+     * @type {Renderer}
+     */
+    var Renderer = Smila.Renderer = function(domId, options){
+        if (!isDefined(options)) {
+            options = {};
+        }
+        var parent = document.getElementById(domId);
+        var canvas = document.createElement('canvas');
+        canvas.height = parent.clientHeight;
+        canvas.width = parent.clientWidth;
+        this.renderItems = [];
+        this.mousePosition = {
+            x: 0,
+            y: 0
+        }
+        this.dimension = {
+            w: parent.clientWidth,
+            h: parent.clientHeight
+        };
+        this.context = canvas.getContext('2d');
+        this._ispaused = false;
+        this.lastTime = -1;
+        this.thread = -1;
+        this.sortThread = -1;
+        parent.appendChild(canvas);
+
+        if ("trackMouse" in options && options.trackMouse) {
+            var self = this;
+            canvas.onmousemove = function(evt){
+                var rect = canvas.getBoundingClientRect();
+                self.mousePosition.x = evt.clientX - rect.left;
+                self.mousePosition.y = evt.clientY - rect.top;
+            };
+        }
+
+        // start
+        startUpdate(this);
+    };
+
+    /**
+     * Run the update loop (technically we need to do this like here
+     * because raf must run in window context...
+     * @param renderer
+     */
+    function startUpdate(renderer) {
+
+        function update(){
+            var now = Date.now(); // afaik faster than performance.now
+            var elapsed = 0;
+            if (renderer.lastTime !== -1){
+                elapsed = now - renderer.lastTime;
+            }
+            renderer.lastTime = now;
+            var dt = elapsed / EXPECTED_ELAPSED_MILLIS;
+            var context = renderer.context;
+            var renderItems = renderer.renderItems;
+
+            for(var i = 0; i < renderItems.length; i+=1){
+                var d = renderItems[i];
+                if (d.update){
+                    d.update(dt, elapsed);
+                }
+                // check, if in frame!
+                d.render(context);
+            }
+
+            if (!renderer._ispaused){
+                renderer.thread = requestAnimationFrame(update);
+            }
+        };
+        renderer.thread = requestAnimationFrame(update);
+        function sort(){
+            // Insertionsort
+            var renderItems = renderer.renderItems;
+            var current, j;
+            for (var i = 1; i < renderItems.length; i++) {
+                current = renderItems[i].z;
+                j = i;
+                while(j > 0 && renderItems[j-1].z > current) {
+                    renderItems[j] = renderItems[j-1];
+                    j = j-1;
+                }
+                renderItems[j] = current;
+            }
+
+            if (!renderer._ispaused){
+                renderer.sortThread = setTimeout(sort, 250);
+            }
+        };
+        renderer.sortThread = setTimeout(sort, 250);
+    };
+
+    /**
+     * stops the update process completely
+     */
+    Renderer.prototype.pause = function(){
+        this._ispaused = true;
+        cancelAnimationFrame(this.thread);
+        clearTimeout(this.sortThread);
+    };
+
+    /**
+     * resumes the previously paused update process
+     */
+    Renderer.prototype.resume = function(){
+        if (!this._ispaused) {
+            throw "Smila: Cannot resume unpaused Renderer!";
+        }
+        cancelAnimationFrame(this.thread);
+        clearTimeout(this.sortThread);
+        this._ispaused = false;
+        startUpdate(this);
+    };
+
+    /**
+     * adds a renderable item
+     * @param sprite
+     */
+    Renderer.prototype.add = function(sprite) {
+        this.renderItems.push(sprite);
+    };
+
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     // Sprite
     // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
+    var Sprite = Smila.Sprite = function(){
 
-    // POLYFILLS
+    };
 
-    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
-        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-    window.requestAnimationFrame = requestAnimationFrame;
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // DataStore
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+
+    var spriteCache = {};
+
+    var DataStore = Smila.DataStore = {
+
+        /**
+         *
+         * @param spriteData: Either:
+         * { key: "test", src: "res/img/test.png" }
+         * Or:
+         * [
+         *      { key: "test", src: "res/img/test.png" },
+         *      { key: "test1", src: "res/img/test2.png" }
+         * ]
+         */
+        put : function(spriteData, callback){
+            var acc = [];
+            if (spriteData instanceof Array) {
+                spriteData.forEach(function(element){
+                   loadSprite(element, function () {
+                       acc.push(0);
+                       if (acc.length === spriteData.length){
+                           callback();
+                       }
+                   }, function(){
+                       throw "Cannot load url: " + element.src;
+                   })
+                });
+            } else {
+                loadSprite(spriteData, callback, function(){
+                    throw "Cannot load url: " + spriteData.src;
+                });
+            }
+        }
+    };
+
+    /**
+     * private helper function to load images
+     */
+    function loadSprite(spriteData, callback, error){
+        log("loading Sprite {" + spriteData.key + "}");
+        var img = new Image();
+        img.onload = function () {
+            log("loaded Sprite {" + spriteData.key + "}");
+            var canvas = document.createElement('canvas');
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var context = canvas.getContext('2d');
+            context.drawImage(img, 0, 0);
+            spriteCache[spriteData.key] = canvas;
+            callback.call(DataStore);
+        };
+        img.onerror = function(){
+            log("Could not load Sprite {" + spriteData.key + "}");
+        }
+
+        if (spriteData.key in spriteCache) {
+            log("Load Sprite " + spriteData.key +" from Cache");
+            callback.call(DataStore);
+        } else if (typeof spriteData.base64 !== "undefined") {
+            log("Load Sprite " + spriteData.key +" from base64");
+            img.src = spriteData.base64;
+        } else {
+            log("Load Sprite " + spriteData.key +" from URL: {" + spriteData.src + "}");
+            img.src = spriteData.src;
+        }
+    };
 
     return Smila;
 }();
+// http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+
+// requestAnimationFrame polyfill by Erik Möller. fixes from Paul Irish and Tino Zijdel
+
+// MIT license
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame']
+            || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); },
+                timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
